692842a85abbdf3634d8198f67443c75e7ba98d8
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-05-25 18:45:32 +0600

    Hello World Print

---
+++ C-Sharp/C-Sharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World!");
+            //var userName = Console.ReadLine();
+            //Console.WriteLine("Username:" + userName);
+            //var password = Console.ReadLine();
+            //Console.WriteLine("Password:" + password);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
