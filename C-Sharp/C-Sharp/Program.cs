2c401c0e1ddbf1c1df122bd4bd29a0411749aa76
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-05-25 23:54:06 +0600

    Value type

--- C-Sharp/C-Sharp/Program.cs
+++ C-Sharp/C-Sharp/Program.cs
@@ -10,14 +10,20 @@ namespace C_Sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,World!");
-            var userName = Console.ReadLine();
-            Console.WriteLine("Username:" + userName);
-            var password = Console.ReadLine();
-            Console.WriteLine("Password:" + password);
-            Console.ReadKey();
+            //Console.WriteLine("Hello,World!");
+            //var userName = Console.ReadLine();
+            //Console.WriteLine("Username:" + userName);
+            //var password = Console.ReadLine();
+            //Console.WriteLine("Password:" + password);
+            //Console.ReadKey();
 
-
+            int ageTisha = 17;
+            int ageSajib;
+            ageSajib = ageTisha;
+            ageTisha = 24;
+            Console.WriteLine("Tisha is "+ageTisha+" years old");
+            Console.WriteLine("Sajib is "+ageSajib+" years old");
+            Console.Read();
 
 
         }
