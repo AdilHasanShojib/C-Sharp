da4e4e0cd4595904683d104be5dd3ad98360bfdd
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-05-26 10:13:18 +0600

    Enumeration Type

--- C-Sharp/C-Sharp/Program.cs
+++ C-Sharp/C-Sharp/Program.cs
@@ -6,54 +6,113 @@ using System.Threading.Tasks;
 
 namespace C_Sharp
 {
-    internal class Program
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //Console.WriteLine("Hello,World!");
+    //        //var userName = Console.ReadLine();
+    //        //Console.WriteLine("Username:" + userName);
+    //        //var password = Console.ReadLine();
+    //        //Console.WriteLine("Password:" + password);
+    //        //Console.ReadKey();
+
+
+    //        //Value Type
+    //        //int ageTisha = 17;
+    //        //int ageSajib;
+    //        //ageSajib = ageTisha;
+    //        //ageTisha = 24;
+    //        //Console.WriteLine("Tisha is "+ageTisha+" years old");
+    //        //Console.WriteLine("Sajib is "+ageSajib+" years old");
+    //        //Console.Read();
+
+
+    //        //Reference Type
+    //        //int[] arr = { 1, 2, 3 };
+    //        //int[] arr2 = arr;
+    //        //Console.WriteLine("arr[0]="+arr[0]);
+    //        //Console.WriteLine("arr2[0]=" + arr2[0]);
+    //        //arr[0] = 60;
+    //        //Console.WriteLine("arr[0]= " + arr[0]);
+    //        //Console.WriteLine("arr2[0]=" + arr2[0]);
+    //        //Console.ReadKey();
+
+    //        //int num1 = 30;
+    //        //int num2 = 50;
+    //        //int sum = num1 + num2;
+    //        //Console.WriteLine("num1:{0} + num2:{1} = sum: {2}",num1,num2,sum);
+    //        //Console.ReadKey();
+
+    //        //precision
+
+    //        //float singlePrecision = 1.342273324738749387298f;
+    //        //double doublePrecision = 3.14161722173732732732732837237d;
+    //        //decimal higherPrecision = 112213232166;
+
+
+    //        //Console.WriteLine("num1:{0},num2:{1},num3: {2}", singlePrecision,doublePrecision,higherPrecision);
+    //        //Console.ReadKey();
+
+    //    }
+    //}
+
+    
+    public enum DayOfWeek // Enumeration Type (enum)
     {
+
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    public class DaysOfWeek
+    {
+
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello,World!");
-            //var userName = Console.ReadLine();
-            //Console.WriteLine("Username:" + userName);
-            //var password = Console.ReadLine();
-            //Console.WriteLine("Password:" + password);
-            //Console.ReadKey();
+          DayOfWeek today= DayOfWeek.Friday;
 
+            if (today == DayOfWeek.Friday || today==DayOfWeek.Saturday)
+            {
+                Console.WriteLine("It is weekend!");
+            }
 
-            //Value Type
-            //int ageTisha = 17;
-            //int ageSajib;
-            //ageSajib = ageTisha;
-            //ageTisha = 24;
-            //Console.WriteLine("Tisha is "+ageTisha+" years old");
-            //Console.WriteLine("Sajib is "+ageSajib+" years old");
-            //Console.Read();
+            else
+            {
+                Console.WriteLine("It is weekday");
+            }
 
-
-            //Reference Type
-            //int[] arr = { 1, 2, 3 };
-            //int[] arr2 = arr;
-            //Console.WriteLine("arr[0]="+arr[0]);
-            //Console.WriteLine("arr2[0]=" + arr2[0]);
-            //arr[0] = 60;
-            //Console.WriteLine("arr[0]= " + arr[0]);
-            //Console.WriteLine("arr2[0]=" + arr2[0]);
-            //Console.ReadKey();
-
-            //int num1 = 30;
-            //int num2 = 50;
-            //int sum = num1 + num2;
-            //Console.WriteLine("num1:{0} + num2:{1} = sum: {2}",num1,num2,sum);
-            //Console.ReadKey();
-
-            //precision
-
-            float singlePrecision = 1.342273324738749387298f;
-            double doublePrecision = 3.14161722173732732732732837237d;
-            decimal higherPrecision = 112213232166;
-
-
-            Console.WriteLine("num1:{0},num2:{1},num3: {2}", singlePrecision,doublePrecision,higherPrecision);
             Console.ReadKey();
 
+
         }
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
