44d8dab22fa5c651a380157024d8e12bab006cf8
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-05-26 09:49:22 +0600

    precision type printing

--- C-Sharp/C-Sharp/Program.cs
+++ C-Sharp/C-Sharp/Program.cs
@@ -29,15 +29,30 @@ namespace C_Sharp
 
 
             //Reference Type
-            int[] arr = { 1, 2, 3 };
-            int[] arr2 = arr;
-            Console.WriteLine("arr[0]="+arr[0]);
-            Console.WriteLine("arr2[0]=" + arr2[0]);
-            arr[0] = 60;
-            Console.WriteLine("arr[0]= " + arr[0]);
-            Console.WriteLine("arr2[0]=" + arr2[0]);
-            Console.ReadKey();
+            //int[] arr = { 1, 2, 3 };
+            //int[] arr2 = arr;
+            //Console.WriteLine("arr[0]="+arr[0]);
+            //Console.WriteLine("arr2[0]=" + arr2[0]);
+            //arr[0] = 60;
+            //Console.WriteLine("arr[0]= " + arr[0]);
+            //Console.WriteLine("arr2[0]=" + arr2[0]);
+            //Console.ReadKey();
 
+            //int num1 = 30;
+            //int num2 = 50;
+            //int sum = num1 + num2;
+            //Console.WriteLine("num1:{0} + num2:{1} = sum: {2}",num1,num2,sum);
+            //Console.ReadKey();
+
+            //precision
+
+            float singlePrecision = 1.342273324738749387298f;
+            double doublePrecision = 3.14161722173732732732732837237d;
+            decimal higherPrecision = 112213232166;
+
+
+            Console.WriteLine("num1:{0},num2:{1},num3: {2}", singlePrecision,doublePrecision,higherPrecision);
+            Console.ReadKey();
 
         }
     }
