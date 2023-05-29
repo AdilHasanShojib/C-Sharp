18b9b95d4a5ba341d78b1583b796684da83227c6
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-05-29 22:34:56 +0600

    Increment Decrement

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -11,27 +11,40 @@ namespace Operators
         static void Main(string[] args)
 
         {
-            int num1 = 10;
-            int num2 = 100;
+            //int num1 = 10;
+            //int num2 = 100;
 
-            int add = num2 + num1;
-            int sub=num2 - num1;
-            int mult=num2*num1;
-            int div=num2/num2;
-            int mod=num2%num1;
+            //int add = num2 + num1;
+            //int sub=num2 - num1;
+            //int mult=num2*num1;
+            //int div=num2/num2;
+            //int mod=num2%num1;
 
-            Console.WriteLine($"ADD:- {add} \n SUB:- {sub} \n MULT:- {mult} \n DIV:- {div} \n MOD:- {mod} \n");
-            
+            //Console.WriteLine($"ADD:- {add} \n SUB:- {sub} \n MULT:- {mult} \n DIV:- {div} \n MOD:- {mod} \n");
 
-            var num3 = 4;
-            var num4 = 49;
-            var num5 = 40;
-            var num6 = 455;
-            var num7 = 41;
 
-            var checkPrecedence = num3 + num6 * (num7 - num3) + num5 + num4 / num3;
-            Console.WriteLine("Result:- " + checkPrecedence);
+            //var num3 = 4;
+            //var num4 = 49;
+            //var num5 = 40;
+            //var num6 = 455;
+            //var num7 = 41;
+
+            //var checkPrecedence = num3 + num6 * (num7 - num3) + num5 + num4 / num3;
+            //Console.WriteLine("Result:- " + checkPrecedence);
+            ////BODMAS (BRACKET,ORDER,DIVISION,MULTIPICATION,ADD,SUB)
+            //Console.ReadKey();
+
+
+            int x = 10;
+            int y = 20;
+            int a = x++;
+            int b = --y;
+            Console.WriteLine($"A:- {x} \n B:- {y}");
             Console.ReadKey();
+
+
+
+
 
 
         }
