86704c63c9b0d5eb0f818b3cec7be01d9d3870d5
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-06-02 20:26:47 +0600

    Multi Nested if else

---
+++ Decision Making and Repeat Operation/Decision Making & Repeat Operation/Decision Making & Repeat Operation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decision_Making___Repeat_Operation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // simple if else Condition 
+
+            int voteCount = 0;
+            string result = null;
+            voteCount = 10;
+
+            if ( voteCount > 0) {
+
+                Console.WriteLine("Won");
+
+            }
+
+            else { Console.WriteLine("Lost"); }
+
+            var vote = voteCount > 0 ? "Won" : "Lost";
+            Console.WriteLine(vote);
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
