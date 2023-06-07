819bae4a7807f4fb882ea1f8516c2513bfdb8d78
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-06-07 21:35:41 +0600

    FizzBuzz printing

--- Decision Making and Repeat Operation/Decision Making & Repeat Operation/Decision Making & Repeat Operation/Program.cs
+++ Decision Making and Repeat Operation/Decision Making & Repeat Operation/Decision Making & Repeat Operation/Program.cs
@@ -237,22 +237,52 @@ namespace Decision_Making___Repeat_Operation
             //    Console.WriteLine($"Odd Number is {i}");
             //}
 
-            int num = 123456;
-            int revNum = 0;
+            //int num = 123456;
+            //int revNum = 0;
 
-            while (num != 0) {
-            
-               int mod=num % 10;
-                revNum = revNum * 10 + mod;
-                num = num / 10;
+            //while (num != 0) {
 
-            
-            
-            
-            }
+            //   int mod=num % 10;
+            //    revNum = revNum * 10 + mod;
+            //    num = num / 10;
 
 
-            Console.WriteLine(revNum);
+
+
+            //}
+
+
+            //Console.WriteLine(revNum);
+
+
+            int i = 0;
+            do
+            {
+                if(i%3==0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if(i%3==0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+                i++;
+
+            } while (i <=100);
+
+
             Console.ReadKey();
 
 
