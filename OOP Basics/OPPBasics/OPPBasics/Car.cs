b9b8246eabd8fc8916174feaebd9cbd8e1d0c328
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-06-10 22:53:21 +0600

    attribute

---
+++ OOP Basics/OPPBasics/OPPBasics/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPPBasics
+{      
+    public enum Color
+    {
+
+        Red,
+        Green,
+        Blue,
+        Yellow     
+
+
+    }
+
+
+    internal class Car
+    {
+
+        public Color color;
+        public string name;
+        public string brand;
+        public DateTime year;
+
+        public static int price;
+    }
+}
---
+++ OOP Basics/OPPBasics/OPPBasics/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OPPBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Car audi = new Car();
+            //audi.color = Color.Red;
+            //Console.WriteLine(audi.color);
+
+            int bmw = Car.price;
+            bmw = 4;
+            Console.WriteLine(bmw);
+
+
+
+
+        }
+    }
+}
