48fb390a1bfc2e1914644a40ac7eabbe97afe05f
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-06-19 18:49:50 +0600

    Inheritance and override and virtual

---
+++ Advance OOP part 1/Advance OOP part 1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Advance_OOP_part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Duck duck = new Duck("Donald","Black","fish");
+            duck.Fly();
+            duck.Eat();
+
+
+
+
+
+
+
+
+        }
+    }
+}
