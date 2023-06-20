50b7c4c2202d73ec688ceedcd89362b56e330cda
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-06-20 18:38:31 +0600

    Interface

---
+++ Advanced OOP Part 2/Advanced OOP Part 2/Bird.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_OOP_Part_2
+{
+    internal class Bird : IAnimal
+    {
+        void IAnimal.Makesound()
+        {
+            Console.WriteLine("Chirp!");
+        }
+
+        void IAnimal.Move()
+        {
+            Console.WriteLine("Flying in the sky!");
+        }
+    }
+}
---
+++ Advanced OOP Part 2/Advanced OOP Part 2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Advanced_OOP_Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal[] animals =
+            {
+                new Bird (),
+                new Dog (),
+                new Cat (),
+            };
+
+            foreach (IAnimal animal in animals)
+            {
+                animal.Makesound();
+                animal.Move();
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
