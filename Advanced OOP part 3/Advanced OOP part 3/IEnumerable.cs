7798da457b0ee5b15abf91e632a08075dca179f6
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-06-26 09:50:59 +0600

    IEnumerable Interface

---
+++ Advanced OOP part 3/Advanced OOP part 3/IEnumerable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_OOP_part_3
+{
+    internal class IEnumerable
+    {
+
+        public static void CollectionSum(IEnumerable<int> collection)
+        {
+
+            int sum = 0;
+            foreach (int item in collection)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine("Sum is {0}",sum);
+
+
+        }
+    }
+}
--- Advanced OOP part 3/Advanced OOP part 3/Program.cs
+++ Advanced OOP part 3/Advanced OOP part 3/Program.cs
@@ -7,11 +7,18 @@
             //DateIO.CheckDate();
             //REX.VerifyEmail();
 
-            BuildInterface SE = new BuildInterface(120);
+            //BuildInterface SE = new BuildInterface(120);
 
-            BuildInterface ML= new BuildInterface(1220);
+            //BuildInterface ML= new BuildInterface(1220);
 
-            Console.WriteLine(SE.Equals(ML));
+            //Console.WriteLine(SE.Equals(ML));
+
+
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] arr = { 1, 2, 3, 4, };
+            //IEnumerable.CollectionSum(list);
+            IEnumerable.CollectionSum(arr);
+
 
 
         }
