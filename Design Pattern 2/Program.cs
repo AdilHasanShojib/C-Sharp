3e9401571ee7ed24e7976d0a9abf2a071a96ef7c
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-07-27 13:03:43 +0600

    PROTOTYPE

---
+++ Design Pattern 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Design_Pattern_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientCodeForUsingPrototype.useprototype();
+        }
+    }
+}
